e7b44cdab76d484e570274a0d248a79639e6be34
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-05-04 23:39:01 +0900

    video 66 Model Company

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -0,0 +1,38 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    private String address;
+
+    private String logo;
+
+    private Instant createdAt;
+
+    private Instant updateAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+}
