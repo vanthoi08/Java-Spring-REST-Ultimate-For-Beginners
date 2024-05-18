e485f2df2754f698f43fe0964c93d242a77c63a8
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-05-18 10:30:03 +0900

    Set Cookies Part 1

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -45,7 +45,7 @@ public class AuthController {
 
         // create a token
         // Không truyền password
-        String access_token = this.securityUtil.createToken(authentication);
+        String access_token = this.securityUtil.createAccessToken(authentication);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         ResLoginDTO res = new ResLoginDTO();
@@ -60,6 +60,9 @@ public class AuthController {
         }
 
         res.setAccessToken(access_token);
+
+        // create refresh token
+        String refresh_token = this.securityUtil.createRefreshToken(loginDTO.getUsername(), res);
         return ResponseEntity.ok().body(res);
 
     }
