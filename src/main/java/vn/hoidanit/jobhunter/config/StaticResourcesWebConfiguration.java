1024f9d6e9e98add39399d2feecb640cee35b239
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-04 23:08:46 +0900

    #100 Upload File Part 1

--- src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/StaticResourcesWebConfiguration.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourcesWebConfiguration
         implements WebMvcConfigurer {
-    @Value("${hoidanit.upload-file.base-path}")
-    private String basePath;
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/storage/**")
-                .addResourceLocations(basePath);
+                .addResourceLocations(baseURI);
     }
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.hoidanit.jobhunter.service.FileService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class FileController {
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseURI;
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/files")
+    public String upload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("folder") String folder) throws URISyntaxException, IOException {
+
+        // skip validate
+
+        // create a directory if not exist
+        this.fileService.createDirectory(baseURI + folder);
+        // store file
+        this.fileService.store(file, folder);
+
+        return file.getOriginalFilename() + folder;
+    }
+}
