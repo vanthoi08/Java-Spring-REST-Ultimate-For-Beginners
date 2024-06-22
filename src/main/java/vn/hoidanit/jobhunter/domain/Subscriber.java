c11a544c9c362a4d1122f883f2e89aace533e9f0
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-22 15:39:45 +0900

    #120 Model Subscribers

--- src/main/java/vn/hoidanit/jobhunter/domain/Subscriber.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Subscriber.java
@@ -3,13 +3,15 @@ package vn.hoidanit.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -20,13 +22,17 @@ import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @Entity
-@Table(name = "skills")
-@Getter
+@Table(name = "subscribers")
 @Setter
-public class Skill {
+@Getter
+public class Subscriber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "email không được để trống")
+    private String email;
+
     @NotBlank(message = "name không được để trống")
     private String name;
 
@@ -35,13 +41,10 @@ public class Skill {
     private String createdBy;
     private String updatedBy;
 
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    List<Job> jobs;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    List<Subscriber> subscribers;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "subscribers" }) // bỏ đi thuộc tính subscribers để tránh vòng lặp vô hạn
+    @JoinTable(name = "subscriber_skill", joinColumns = @JoinColumn(name = "subscriber_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private List<Skill> skills;
 
     @PrePersist
     public void handleBeforeCreate() {
@@ -60,5 +63,4 @@ public class Skill {
         this.updatedAt = Instant.now();
 
     }
-
 }
