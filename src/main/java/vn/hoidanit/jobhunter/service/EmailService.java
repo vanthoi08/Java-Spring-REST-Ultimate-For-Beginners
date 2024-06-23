abf623011eaf66f4340bf971e9d9e50bfd640aa3
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-23 12:22:20 +0900

    #126 Send Email With template

--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
@@ -13,17 +14,22 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import vn.hoidanit.jobhunter.domain.Job;
+import vn.hoidanit.jobhunter.repository.JobRepository;
 
 @Service
 public class EmailService {
     private final MailSender mailSender;
     private final JavaMailSender javaMailSender;
     private final SpringTemplateEngine templateEngine;
+    private final JobRepository jobRepository;
 
-    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine) {
+    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine,
+            JobRepository jobRepository) {
         this.mailSender = mailSender;
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
+        this.jobRepository = jobRepository;
     }
 
     public void sendSimpleEmail() {
@@ -52,6 +58,11 @@ public class EmailService {
 
     public void sendEmailFromTemplateSync(String to, String subject, String templateName) {
         Context context = new Context();
+        List<Job> arrJob = this.jobRepository.findAll();
+        String name = "ERIC";
+        context.setVariable("name", name);
+        context.setVariable("jobs", arrJob);
+
         String content = this.templateEngine.process(templateName, context);
         this.sendEmailSync(to, subject, content, false, true);
     }
