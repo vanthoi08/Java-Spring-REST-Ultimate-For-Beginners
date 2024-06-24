44d3ec7a12fa40e8c6bac39f94a92a1da68fd0d7
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-24 22:22:34 +0900

    #131 Xu Ly Global Exception

--- src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
@@ -19,6 +19,16 @@ import vn.hoidanit.jobhunter.domain.response.RestResponse;
 @RestControllerAdvice
 
 public class GlobalException {
+    // handle all exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestResponse<Object>> handleAllException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        res.setMessage(ex.getMessage());
+        res.setError("Internal Server Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+    }
+
     @ExceptionHandler(value = {
             UsernameNotFoundException.class,
             BadCredentialsException.class,
